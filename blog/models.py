f4bf90408efef35c3095784cd98cc5eb3c739b29
Ravshanjon <akhmadjonovr.98@gmail.com>
2024-02-02 20:01:36 +0500

    feat: app created

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models import CASCADE
+
+
+class AbstractModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractUser, AbstractModel):
+    avatar = models.ImageField(upload_to='avatars')
+
+
+class Post(AbstractModel):
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    published = models.DateField()
+    is_active = models.BooleanField(default=False)
+    author = models.ForeignKey("blog.User", CASCADE, "posts")
