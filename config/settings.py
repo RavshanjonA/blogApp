5cc49557c4cc2ad6b1ff7f6a7a028b82d8397dd8
Ravshanjon <akhmadjonovr.98@gmail.com>
2024-02-12 20:48:47 +0500

    feat: added

--- config/settings.py
+++ config/settings.py
@@ -103,6 +103,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'staticfiles'
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
